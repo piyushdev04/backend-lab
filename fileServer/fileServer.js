fa92336282b85025965110d94618dd4a64e59c6a
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-10 19:10:23 +0530

    Create fileServer.js

---
+++ fileServer/fileServer.js
@@ -0,0 +1,36 @@
+const { error } = require('console');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+
+app.get('/files', function (req, res) {
+    fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'Failed to retrieve files' });
+        }
+        res.json(files);
+    });
+});
+
+app.get('/file/:filename', function (req, res) {
+    const filepath = path.join(__dirname, './files/', req.params.filename);
+
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if(err) {
+            return res.status(404).send('File not found');
+        }
+        res.send(data);
+    });
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('Route not found');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
