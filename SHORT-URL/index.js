63bb571abb4cc785f27e6de56e5c0f00c0b2f34d
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-24 23:31:49 +0530

    Create index.js

---
+++ SHORT-URL/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require('./models/url');
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+connectToMongoDB(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.use(express.json());
+app.use("/url", urlRoute);
+
+app.get('/:shortId', async(req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
