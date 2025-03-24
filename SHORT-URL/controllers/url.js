77e7b6cf945c0b2f11afdb86288c4b1f67262594
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-24 23:31:45 +0530

    Create url.js

---
+++ SHORT-URL/controllers/url.js
@@ -0,0 +1,30 @@
+const shortid = require("shortid");
+const URL = require('../models/url');
+
+async function handleGenerateNewShortURL(req, res) {
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: 'URL is required' });
+
+    const shortId = shortid.generate();
+    await URL.create({
+        shortId: shortId,
+        redirectURL: body.url,
+        visitHistory: [],
+    });
+
+    return res.json({ id: shortId });
+}
+
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+    return res.json({
+        totalClicks: result.visitHistory.length,
+        handleGetAnalytics: result.visitHistory,
+    });
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+    handleGetAnalytics,
+};
