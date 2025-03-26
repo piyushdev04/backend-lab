15279028b4636df657c65901eb3ad2d4bd9a23d1
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-26 23:21:54 +0530

    Update index.js

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require('path');
 const { connectToMongoDB } = require("./connect");
+const staticRoute = require('./routes/staticRouter');
 const urlRoute = require("./routes/url");
 const URL = require('./models/url');
 
@@ -17,14 +18,10 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/test", async (req, res) => {
-  const allUrls = await URL.find({});
-  return res.render("home", {
-    urls: allUrls,
-  });
-});
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get('/url/:shortId', async(req, res) => {
   const shortId = req.params.shortId;
