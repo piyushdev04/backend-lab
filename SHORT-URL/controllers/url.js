20f0c3b834b0b4f0de6be92f8feb3d33039ab5f7
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-26 23:21:49 +0530

    Update url.js

--- SHORT-URL/controllers/url.js
+++ SHORT-URL/controllers/url.js
@@ -12,7 +12,9 @@ async function handleGenerateNewShortURL(req, res) {
         visitHistory: [],
     });
 
-    return res.json({ id: shortId });
+    return res.render("home", {
+        id: shortId
+    });
 }
 
 async function handleGetAnalytics(req, res) {
