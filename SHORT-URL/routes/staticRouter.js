67f35f224c5c269cc6b82277d896cdefbd02d961
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-03-27 14:07:10 +0530

    Update staticRouter.js

--- SHORT-URL/routes/staticRouter.js
+++ SHORT-URL/routes/staticRouter.js
@@ -4,12 +4,19 @@ const URL = require("../models/url");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    const allurls = await URL.find({});
+    if (!req.user) return res.redirect("/login");
+    const allurls = await URL.find({ createdBy: req.user._id });
     return res.render("home", {
         urls: allurls,
     });
 });
 
-module.exports = router;
+router.get("/signup", (req, res) =>{
+    return res.render("signup");
+});
 
-// Learning EJS and server-side rendering
+router.get("/login", (req, res) => {
+    return res.render("login");
+});
+
+module.exports = router;
