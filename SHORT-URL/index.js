cf6ceb376ab5c18107ccd723813644ec5f43a9fd
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-04 23:56:25 +0530

    Update index.js

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require('path');
 const cookieParser = require("cookie-parser");
 const { connectToMongoDB } = require("./connect");
-const { restrictToLoggedinUserOnly, checkAuth } = require("./middleware/auth");
+const { checkForAuthentication, restrictTo} = require("./middleware/auth");
 const staticRoute = require('./routes/staticRouter');
 const urlRoute = require("./routes/url");
 const userRoute = require("./routes/user");
@@ -23,10 +23,11 @@ app.set("views", path.resolve("./views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(checkForAuthentication);
 
-app.use("/url", restrictToLoggedinUserOnly ,urlRoute);
+app.use("/url", restrictTo(["NORMAL"]) ,urlRoute);
 app.use("/user", userRoute);
-app.use("/", checkAuth, staticRoute);
+app.use("/", staticRoute);
 
 app.get('/url/:shortId', async(req, res) => {
   const shortId = req.params.shortId;
