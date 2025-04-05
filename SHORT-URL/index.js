0a346317e908bcc254186d27356882fb895ae37e
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-05 20:44:04 +0530

    Update index.js

--- SHORT-URL/index.js
+++ SHORT-URL/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthentication);
 
-app.use("/url", restrictTo(["NORMAL"]) ,urlRoute);
+app.use("/url", restrictTo(["NORMAL", "ADMIN"]) ,urlRoute);
 app.use("/user", userRoute);
 app.use("/", staticRoute);
 
