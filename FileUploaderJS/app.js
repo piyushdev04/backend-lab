ce19548b28086f5076ac1d28db3ca5b4cbbe5c7b
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-14 23:30:18 +0530

    Create app.js

---
+++ FileUploaderJS/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const uploadRoutes = require('./routes/upload');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static('uploads'));
+
+app.use('/api', uploadRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
