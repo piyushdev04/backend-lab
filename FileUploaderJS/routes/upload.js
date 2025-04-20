c8f85244b59914bf30d4642665b09e10c5c0532a
Piyush Bafna <130243298+piyushdev04@users.noreply.github.com>
2025-04-20 23:10:46 +0530

    Update upload.js

--- FileUploaderJS/routes/upload.js
+++ FileUploaderJS/routes/upload.js
@@ -2,40 +2,49 @@ const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
-const { error } = require('console');
 const router = express.Router();
 
-// multer config
+// Multer storage configuration
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
 });
+
 const upload = multer({ storage: storage });
 
-// upload route
+// File upload route
 router.post('/upload', upload.single('file'), (req, res) => {
-    res.json({ filename: req.file.filename, path: `/uploads/${req.file.filename}`});
+  if (req.file) {
+    // Send response with file information
+    res.json({
+      filename: req.file.filename,
+      path: `/uploads/${req.file.filename}`,
+      message: 'File uploaded successfully.',
+    });
+  } else {
+    res.status(400).json({ error: 'No file uploaded' });
+  }
 });
 
-// get list of files
+// Get the list of uploaded files
 router.get('/files', (req, res) => {
-    fs.readdir('./uploads', (err, files) => {
-        if (err) return res.status(500).json({ error: err });
-        res.json(files);
-    });
+  fs.readdir('./uploads', (err, files) => {
+    if (err) return res.status(500).json({ error: err });
+    res.json(files);
+  });
 });
 
-// delete file
+// Delete a file route
 router.delete('/delete/:filename', (req, res) => {
-    const filePath = path.join(__dirname, '../uploads', res.params.filename);
-    fs.unlink(filePath, err => {
-        if(err) return res.status(500).json({ error: 'File not found' });
-        res.json({ message: 'File deleted' });
-    });
+  const filePath = path.join(__dirname, '../uploads', req.params.filename);
+  fs.unlink(filePath, (err) => {
+    if (err) return res.status(500).json({ error: 'File not found' });
+    res.json({ message: 'File deleted' });
+  });
 });
 
 module.exports = router;
